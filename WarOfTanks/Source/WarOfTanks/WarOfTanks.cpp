f43a26fa5397f09193db3506861e87ffab613ca5
Joakim94 <barkatishak@hotmail.com>
2020-03-30 19:03:00 +0100

    01- Getting Started

---
+++ WarOfTanks/Source/WarOfTanks/WarOfTanks.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WarOfTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarOfTanks, "WarOfTanks" );
